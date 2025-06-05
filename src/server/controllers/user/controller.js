7a5df51f546d43bf5ac61d1624256476a02bf7ed
SubhaNITS150 <subhajyoti_ug_23@cse.nits.ac.in>
2025-06-05 13:39:26 +0530

    Added login functionality backend

--- src/server/controllers/user/controller.js
+++ src/server/controllers/user/controller.js
@@ -1,5 +1,6 @@
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from '@prisma/client';
+import validator from "validator";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -14,14 +15,87 @@ const getUsers = async (req, res) => {
 
 const addUser = async (req, res) => {
     try{
-        const {name, email} = req.body;
-        const user = await prisma.user.create({
-            data: { name, email }
+        const {name, email, password} = req.body;
+
+        if (!name || validator.isEmpty(name.trim())) {
+            return res.status(400).send("Name cannot be empty");
+        }
+        if (!email || validator.isEmpty(email.trim())) {
+            return res.status(400).send("Email Address cannot be empty");
+        }
+        if (!password || validator.isEmpty(password)) {
+            return res.status(400).send("Password cannot be empty");
+        }
+
+        if (!validator.isEmail(email)) {
+            return res.status(400).send("Invalid email");
+        }
+
+        if (!validator.isStrongPassword(password, { minLength: 8 })) {
+            return res.status(400).send("Password is not strong enough. It should be at least 8 characters and include uppercase, lowercase, number, and symbol.");
+        }
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email: email },
         });
-        res.status(200).send(user);
+
+        if (existingUser) {
+            return res.status(400).send("User already exists");
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: { name : name,
+                    email : email.toLowerCase(),
+                    password: hashedPassword,
+            },
+        });
+
+        res.status(200).send(newUser);
+
     } catch (e) {
-        res.status(500).send("An error occured :- ", e);
+        res.status(500).json({ error: e.message });
     }
 }
 
-export {getUsers, addUser};
+const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        if (!email) {
+            return res.send("Email cannot be empty");
+        }
+
+        if (!password) {
+            return res.send("Password cannot be empty");
+        }
+
+        const existingUser = await prisma.user.findUnique(
+            {where : { email: email.toLowerCase() }},
+        )
+
+        if(!existingUser) {
+            return res.send("User does not exists, Sign Up");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+            return res.status(401).send("Invalid password");
+        }
+
+        return res.status(200).json({
+            message: "Login successful",
+            user: {
+                id: existingUser.id,
+                email: existingUser.email,
+                name: existingUser.name,
+            },
+        });
+
+    } catch (e) {
+        return res.json({error: e.message});
+    }
+}
+
+export {getUsers, addUser, loginUser};
--- src/server/routes/user/route.js
+++ src/server/routes/user/route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {getUsers, addUser} from "../../controllers/user/controller.js"
+import {getUsers, addUser, loginUser} from "../../controllers/user/controller.js"
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers)
 userRouter.post("/", addUser)
+userRouter.get("/login", loginUser)
 
 export default userRouter;
